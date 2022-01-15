b3d15560784830edd3e4b7c6674ba31ecfea36dc
박도윤 <doyun0887@gmail.com>
2022-01-15 23:11:35 +0900

    kakao map update

--- KNU_Hackerton/MapController.swift
+++ KNU_Hackerton/MapController.swift
@@ -10,24 +10,37 @@ import UIKit
 
 class MapController:UIViewController {
     
-    private let searchbar:UISearchBar = {
-        let bar = UISearchBar()
-        
-        return bar
-    }()
+    //MARK: - Properties
+    private var isSearchMode:Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
+    
+    private let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var map = MTMapView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        let map = MTMapView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
         
         view.addSubview(map)
+        configureSearchController()
     }
     
     //MARK: - configure
-    
-    func configureUI() {
-        
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a user"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
+}
+extension MapController:UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        print(searchText)
+//        filterdUsers = users.filter({ $0.username.contains(searchText)})
     }
 }
 extension MapController:MTMapViewDelegate {
